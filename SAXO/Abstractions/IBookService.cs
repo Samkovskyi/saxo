7f7f3a8b742e09e8f9085c1bc5435a5b8e2860c5
Taras Samkovskyi <t.samkovskyi@gmail.com>
2015-05-12 00:45:36 +0300

    Added Abstractions

---
+++ SAXO/Abstractions/IBookService.cs
@@ -0,0 +1,14 @@
+﻿using SAXO.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SAXO.Abstractions
+{
+    public interface IBookService
+    {
+        IEnumerable<Book> GetBooks(IEnumerable<String> ids);
+
+    }
+}
